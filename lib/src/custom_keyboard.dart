645bfa7584b13a5f274b670727040392048c07b9
kirankamal <kiran.k@lucidplus.com>
2023-08-23 21:31:37 +0530

    special key removed

--- lib/src/custom_keyboard.dart
+++ lib/src/custom_keyboard.dart
@@ -95,26 +95,6 @@ class _CustomKeyBoardState extends State<CustomKeyBoard> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           buildNumberButton(
-            icon: widget.specialKey ??
-                Icon(
-                  Icons.circle,
-                  key: const Key('specialKey'),
-                  color: widget.pinTheme.keysColor,
-                  size: 7,
-                ),
-            onPressed: widget.specialKeyOnTap ??
-                () {
-                  if (widget.controller!.text.length < widget.maxLength!) {
-                    if (!widget.controller!.text.contains(".")) {
-                      setState(() {
-                        widget.controller!.text = widget.controller!.text + ".";
-                      });
-                    }
-                    widget.onChanged!(widget.controller!.text);
-                  }
-                },
-          ),
-          buildNumberButton(
             number: 0,
             onPressed: () {
               if (widget.controller!.text.length < widget.maxLength!) {
